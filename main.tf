10811e9ed1037ed335afb9aedaadfc763b45093f
karol <>
2022-10-26 16:20:25 +0200

    changes

--- main.tf
+++ main.tf
@@ -2,33 +2,38 @@ module "branch1" {
   source  = "terraform-aviatrix-modules/mc-edge/aviatrix"
   version = "1.1.1"
 
-  site_id = "branch1"
+  site_id = var.site_id
   ztp_file_download_path = "${path.module}"
   edge_gws = {
     gw1 = {
-      gw_name                 = "gw1",
-      lan_interface_ip_prefix = "10.50.10.10/24",
-      wan_default_gateway_ip  = "1.1.1.1"
-      wan_interface_ip_prefix = "1.1.1.10/24"
+      management_interface_config = "Static"
+      management_interface_ip = var.management_interface_ip_prefix
+      gw_name                 = var.gw_name
+      management_egress_ip_prefix = var.management_egress_ip_prefix
+      wan_interface_ip_prefix = var.wan_interface_ip_prefix
+      wan_default_gateway_ip  = var.wan_default_gateway_ip
+      lan_interface_ip_prefix = var.lan_interface_ip_prefix
+      management_interface_ip_prefix  = var.management_interface_ip_prefix
+      management_default_gateway_ip  = var.management_default_gateway_ip
+      dns_server_ip = var.dns_server_ip
+      secondary_dns_server_ip = var.dns_server_ip
       ztp_file_download_path = "${path.module}"
       transit_gws = {
         transit1 = {
-          name = "transit1",
+          name = "aws-tgw-eu-central-1",
           #attached = true
         },
       }
     }
   }
 }
-output "path" {
-  value =  "${path.module}/gw1-branch1.iso"
-}
+
 
 resource "aws_s3_object" "cfg_upload" {
   bucket = "aviatrixnow"
-  key    = "gw1-branch1.iso"
-  source = "${path.module}/gw1-branch1.iso"
-#  etag   = filemd5("${path.cwd}/gw1-branch1.iso")
-
+  key    = "${var.gw_name}-${var.site_id}.iso"
+  source = "${path.module}/${var.gw_name}-${var.site_id}.iso"
   depends_on = [module.branch1]
+  acl = "public-read"
+
 }
--- variables.tf
+++ variables.tf
@@ -1,37 +1,58 @@
-#variable "region" {}
-#
-# variable "account_name" {
-#   type        = map(string)
-#   default     = {
-#     "eu-central-1" = "aws-account",
-#     "us-east-1" =   "aws-account",
-#     "West Europe" = "azure-account-sec"
-#   }
-# }
-
 variable "ctrl_password" {}
 
 variable "ctrl_ip" {
   default = ""
 }
 
+variable "gw_name" {
+  default = "gwname"
+}
+
+variable "site_id" {
+  default = "sitename"
+}
+
+variable "management_interface_ip" {
+  default = "10.50.0.1/24"
+}
+
+
+variable "management_interface_config" {
+  default = "Static"
+}
+
+variable "management_egress_ip_prefix" {
+  default = "10.80.0.0/24"
+}
+
+variable "wan_interface_ip_prefix" {
+  default = "10.70.0.2/24"
+}
+
+variable "wan_default_gateway_ip" {
+  default = "10.70.0.1"
+}
+
+variable "lan_interface_ip_prefix" {
+  default = "10.80.0.0/24"
+}
+
+variable "management_interface_ip_prefix" {
+  default = "10.60.0.2/24"
+}
+
+variable "management_default_gateway_ip" {
+  default = "10.60.0.1"
+}
+
+variable "dns_server_ip" {
+  default = "10.60.0.1"
+}
 variable "access_key" {
   default = ""
 }
 
+
 variable "secret_key" {
   default = ""
 }
-
-
-# variable "network_domain" {}
-#
-#
-# variable "transit_gw" {
-#   type        = map(string)
-#   default     = {
-#     "eu-central-1" = "aws-tgw-eu-central-1",
-#     "us-east-1" =   "aws-tgw-us-east-1",
-#     "West Europe" = "az-tgw-west-europe"
-#   }
-# }
